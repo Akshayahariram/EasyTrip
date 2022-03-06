7261d14eb708c64ed187a6e64101eca66a9961b5
akshaya <akshayahariran2782003@gmail.com>
2022-03-06 14:34:23 +0530

    Commit1

--- app/src/main/java/com/hp/easytrip/Otp_Verification.java
+++ app/src/main/java/com/hp/easytrip/Otp_Verification.java
@@ -1,14 +1,13 @@
 package com.hp.easytrip;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+public class Otp_Verification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_otp_verification);
     }
 }
