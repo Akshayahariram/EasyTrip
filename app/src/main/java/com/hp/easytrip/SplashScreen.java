ca39e3452368b6f6de0e63aadcd5cdda25ee9c17
Aswin Hariram <Aswin_pc_hp@outlook.com>
2022-03-06 17:34:34 +0530

    aswin commit

--- app/src/main/java/com/hp/easytrip/SplashScreen.java
+++ app/src/main/java/com/hp/easytrip/SplashScreen.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+import com.hp.easytrip.databinding.ActivitySplashScreenBinding;
 
+public class SplashScreen extends AppCompatActivity {
+    ActivitySplashScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
 
 
         new Handler().postDelayed(new Runnable() {
