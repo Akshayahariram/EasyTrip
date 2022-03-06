1d742d491fac040d4597dc503f2718d7787c0e0c
akshaya <akshayahariran2782003@gmail.com>
2022-03-06 12:58:01 +0530

    Commit1

---
+++ app/src/main/java/com/hp/easytrip/Signup.java
@@ -0,0 +1,14 @@
+package com.hp.easytrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Signup);
+    }
+}
---
+++ app/src/main/java/com/hp/easytrip/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.hp.easytrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+}
