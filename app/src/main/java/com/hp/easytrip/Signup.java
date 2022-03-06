87a8c702701bf0ebf9b414272d6b749b8f90d219
akshaya <akshayahariran2782003@gmail.com>
2022-03-06 15:15:42 +0530

    Commit1

--- app/src/main/java/com/hp/easytrip/Signup.java
+++ app/src/main/java/com/hp/easytrip/Signup.java
@@ -2,13 +2,19 @@ package com.hp.easytrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+import com.hp.easytrip.databinding.ActivitySignupBinding;
 
+public class Signup extends AppCompatActivity {
+    ActivitySignupBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
+
 }
