e74258f3c91cbff429f7e153290284cfa82bef5f
Aswin Hariram <Aswin_pc_hp@outlook.com>
2022-03-07 20:13:23 +0530

    aswin commit

--- app/src/main/java/com/hp/easytrip/Details.java
+++ app/src/main/java/com/hp/easytrip/Details.java
@@ -1,8 +1,12 @@
 package com.hp.easytrip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.hp.easytrip.databinding.ActivityDetailsBinding;
@@ -30,6 +34,7 @@ public class Details extends AppCompatActivity {
         switch (name){
             case "Badrinath":
                 binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.badrniath));
+
                 break; case "Gangotri":
                 binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.gangotri));
                 break; case "Yamunotri":
@@ -38,6 +43,36 @@ public class Details extends AppCompatActivity {
                 binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.khendra));
                 break;
         }
+        binding.toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id==R.id.dir&&name.equals("Badrinath")){
+                    Uri gmmIntentUri = Uri.parse("geo:0,0?q=Badrinath");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                }  if (id==R.id.dir&&name.equals("Gangotri")){
+                    Uri gmmIntentUri = Uri.parse("geo:0,0?q=Gangotri");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                }  if (id==R.id.dir&&name.equals("Yamunotri")){
+                    Uri gmmIntentUri = Uri.parse("geo:0,0?q=Yamunotri");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                } if (id==R.id.dir&&name.equals("Kedarnath")){
+                    Uri gmmIntentUri = Uri.parse("geo:0,0?q=Kedarnath");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                }
+                return false;
+            }
+        });
     }
+
+
 
 }
--- app/src/main/java/com/hp/easytrip/MainActivity.java
+++ app/src/main/java/com/hp/easytrip/MainActivity.java
@@ -1,22 +1,56 @@
 package com.hp.easytrip;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.hp.easytrip.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    double lat, log;
     ActivityMainBinding binding;
+
+    FusedLocationProviderClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+     /*   if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLoca();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+
+        }
+
+        binding.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String geoUri = "http://maps.google.com/maps?q=loc:" + lat + "," + log + " (" + "Current Location" + ")";
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                Uri screenshotUri = Uri.parse(geoUri);
+
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+                startActivity(Intent.createChooser(sharingIntent, "Share image using"));
+            }
+        });*/
 
         binding.policeNearby.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
             }
-        }); binding.policeBulk.setOnClickListener(new View.OnClickListener() {
+        });
+        binding.policeBulk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Uri gmmIntentUri = Uri.parse("geo:0,0?q=petrol");
@@ -34,7 +69,8 @@ public class MainActivity extends AppCompatActivity {
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
             }
-        }); binding.nearbyHotels.setOnClickListener(new View.OnClickListener() {
+        });
+        binding.nearbyHotels.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -46,7 +82,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mapIntent);
 
             }
-        });binding.nearbyLodge.setOnClickListener(new View.OnClickListener() {
+        });
+        binding.nearbyLodge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Uri gmmIntentUri = Uri.parse("geo:0,0?q=lodge");
@@ -58,62 +95,83 @@ public class MainActivity extends AppCompatActivity {
         binding.Kedarnath.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Details.class)
-                .putExtra("NAME","Kedarnath")
-                .putExtra("CONTENT","One of the most revered temple destinations of India, Kedarnath town is nestled in the mighty Garhwal Himalayas. The town, built around the revered Kedarnath temple, is located at an altitude of 3,580 m, near Chorabari glacier, which is the source of the Mandakini river. Dedicated to Lord Shiva, the ancient temple has exquisite architecture and is built of extremely large but evenly shaped grey stone slabs. A conical rock formation inside the temple is worshipped as Lord Shiva in his “Sadashiva” form.  The Kedarnath temple, dedicated to Lord Shiva, is a part of Char Dham pilgrimage circuit, and is one of the 12 Jyotirlingas of Lord Shiva in India. Behind the Kedarnath temple, stand the Kedarnath peak, Kedar Dome and other Himalayan peaks.\n" +
-                        "\n" +
-                        "The historical name of this region is \"Kedar Khand\" and legend says, the Pandavas from the epic Mahabharata, after having defeated the Kauravas, felt guilty of having killed so many people and sought the blessings of Lord Shiva for redemption. The Lord eluded them repeatedly and took refuge at Kedarnath in the form of a bull. The Lord dived into the ground, leaving his hump on the surface at Kedarnath. The remaining portions of Lord Shiva appeared at four other places and are worshipped there as his manifestations. The arms of the Lord appeared at Tungnath, the face at Rudranath, the belly at Madmaheshwar and his locks (hair) at Kalpeshwar. The Kedarnath and four above mentioned shrines make the revered Panch Kedar pilgrimage circuit. For booking a pilgrimage, visit https://badrinath-kedarnath.gov.in/common/kedarnath.\n" +
-                        "\n" +
-                        "BEST TIME TO VISIT\n" +
-                        "\n" +
-                        "From May to October is the best time to visit Kedarnath. During winter months, the town remains closed due to heavy snowfall. The temple has been shut for the winter of 2021-22.\n" +
-                        "\n" +
-                        "Where to stay\n" +
-                        "\n" +
-                        "KEDARNATH (Rudra Meditation Cave) Kedarnath (Swargarohini Complex)\n" +
-                        "\n" +
-                        "Itineraries"));
+                startActivity(new Intent(getApplicationContext(), Details.class)
+                        .putExtra("NAME", "Kedarnath")
+                        .putExtra("CONTENT", "One of the most revered temple destinations of India, Kedarnath town is nestled in the mighty Garhwal Himalayas. The town, built around the revered Kedarnath temple, is located at an altitude of 3,580 m, near Chorabari glacier, which is the source of the Mandakini river. Dedicated to Lord Shiva, the ancient temple has exquisite architecture and is built of extremely large but evenly shaped grey stone slabs. A conical rock formation inside the temple is worshipped as Lord Shiva in his “Sadashiva” form.  The Kedarnath temple, dedicated to Lord Shiva, is a part of Char Dham pilgrimage circuit, and is one of the 12 Jyotirlingas of Lord Shiva in India. Behind the Kedarnath temple, stand the Kedarnath peak, Kedar Dome and other Himalayan peaks.\n" +
+                                "\n" +
+                                "The historical name of this region is \"Kedar Khand\" and legend says, the Pandavas from the epic Mahabharata, after having defeated the Kauravas, felt guilty of having killed so many people and sought the blessings of Lord Shiva for redemption. The Lord eluded them repeatedly and took refuge at Kedarnath in the form of a bull. The Lord dived into the ground, leaving his hump on the surface at Kedarnath. The remaining portions of Lord Shiva appeared at four other places and are worshipped there as his manifestations. The arms of the Lord appeared at Tungnath, the face at Rudranath, the belly at Madmaheshwar and his locks (hair) at Kalpeshwar. The Kedarnath and four above mentioned shrines make the revered Panch Kedar pilgrimage circuit. For booking a pilgrimage, visit https://badrinath-kedarnath.gov.in/common/kedarnath.\n" +
+                                "\n" +
+                                "BEST TIME TO VISIT\n" +
+                                "\n" +
+                                "From May to October is the best time to visit Kedarnath. During winter months, the town remains closed due to heavy snowfall. The temple has been shut for the winter of 2021-22.\n" +
+                                "\n" +
+                                "Where to stay\n" +
+                                "\n" +
+                                "KEDARNATH (Rudra Meditation Cave) Kedarnath (Swargarohini Complex)\n" +
+                                "\n" +
+                                "Itineraries"));
             }
-        }); binding.Yamunotri.setOnClickListener(new View.OnClickListener() {
+        });
+        binding.Yamunotri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Details.class)
-                        .putExtra("NAME","Yamunotri")
-                .putExtra("CONTENT","Yamunotri is a part of Char Dham (along with Gangotri, Kedarnath and Badrinath), the four most revered Hindu pilgrimages in the Himalayas. The small mountain hamlet, with the Yamunotri Temple at its centre, attracts thousands of devotees every year and is the commencing point of the Char Dham Yatra pilgrimage (May to October), which proceeds from Yamunotri to Gangotri and finally to Kedarnath and Badrinath. Lodged in a narrow gorge, close to the source of the Yamuna, the Yamunotri Temple is dedicated to Yamuna, the second-most sacred river after the Ganges. A dip in River Yamuna is said to protect one from untimely death.  Devotees either walk or ride a palanquin or a pony to reach the temple (around 3,233 m above sea level) from Janki Chatti, a steep trek of about 3 km that takes about 3 hours. \n" +
-                        "\n" +
-                        "Best Time To Visit: April to June and September to November"));
+                startActivity(new Intent(getApplicationContext(), Details.class)
+                        .putExtra("NAME", "Yamunotri")
+                        .putExtra("CONTENT", "Yamunotri is a part of Char Dham (along with Gangotri, Kedarnath and Badrinath), the four most revered Hindu pilgrimages in the Himalayas. The small mountain hamlet, with the Yamunotri Temple at its centre, attracts thousands of devotees every year and is the commencing point of the Char Dham Yatra pilgrimage (May to October), which proceeds from Yamunotri to Gangotri and finally to Kedarnath and Badrinath. Lodged in a narrow gorge, close to the source of the Yamuna, the Yamunotri Temple is dedicated to Yamuna, the second-most sacred river after the Ganges. A dip in River Yamuna is said to protect one from untimely death.  Devotees either walk or ride a palanquin or a pony to reach the temple (around 3,233 m above sea level) from Janki Chatti, a steep trek of about 3 km that takes about 3 hours. \n" +
+                                "\n" +
+                                "Best Time To Visit: April to June and September to November"));
             }
-        }); binding.Gangotri.setOnClickListener(new View.OnClickListener() {
+        });
+        binding.Gangotri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Details.class)
-                        .putExtra("NAME","Gangotri")
-                .putExtra("CONTENT","One of the char dhams (the most sacred pilgrimage circuits in northern India with four holy destinations), Gangotri, in Uttarkashi, is a small town with the temple of Goddess Ganga at its heart. A 12-hour drive from Rishikesh, Gangotri is nestled among lofty Garhwal Himalayan peaks, glaciers and dense forests, and is one of highest pilgrimages in India (approx 3,415 m). Other than its divine atmosphere, Gangotri offers stunning vistas all around. According to Hindu legends, the most sacred of all rivers, Ganges (or Ganga), descended from heaven to earth at Gangotri, when Lord Shiva released the mighty river from his locks. The actual origin of the river is at Gaumukh in the Gangotri glacier, 19 km away from Gangotri and is accessible by trekking. After it originates from Gaumukh, the river is known as Bhagirathi and it acquires the name 'Ganga' after the river Alaknanda merges into it near the town of Devaprayag.\n" +
-                        "\n" +
-                        "When to visit\n" +
-                        "Gangotri becomes a bustling hub of devotees during the Char Dham Yatra season (May to October).\n" +
-                        "\n" +
-                        "Where to stay\n" +
-                        "Gangotri (Yatri Niwas)"));
+                startActivity(new Intent(getApplicationContext(), Details.class)
+                        .putExtra("NAME", "Gangotri")
+                        .putExtra("CONTENT", "One of the char dhams (the most sacred pilgrimage circuits in northern India with four holy destinations), Gangotri, in Uttarkashi, is a small town with the temple of Goddess Ganga at its heart. A 12-hour drive from Rishikesh, Gangotri is nestled among lofty Garhwal Himalayan peaks, glaciers and dense forests, and is one of highest pilgrimages in India (approx 3,415 m). Other than its divine atmosphere, Gangotri offers stunning vistas all around. According to Hindu legends, the most sacred of all rivers, Ganges (or Ganga), descended from heaven to earth at Gangotri, when Lord Shiva released the mighty river from his locks. The actual origin of the river is at Gaumukh in the Gangotri glacier, 19 km away from Gangotri and is accessible by trekking. After it originates from Gaumukh, the river is known as Bhagirathi and it acquires the name 'Ganga' after the river Alaknanda merges into it near the town of Devaprayag.\n" +
+                                "\n" +
+                                "When to visit\n" +
+                                "Gangotri becomes a bustling hub of devotees during the Char Dham Yatra season (May to October).\n" +
+                                "\n" +
+                                "Where to stay\n" +
+                                "Gangotri (Yatri Niwas)"));
             }
-        }); binding.Badrinath.setOnClickListener(new View.OnClickListener() {
+        });
+        binding.Badrinath.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Details.class)
-                        .putExtra("NAME","Badrinath")
-                .putExtra("CONTENT","The Badrinath Temple also known as the Badrinarayan Temple, located in Uttarakhand's Badrinath town, is one of the four Char Dhams (four important pilgrimages) in the state. There are four pilgrim-destinations namely Yamunotri, Gangotri, Kedarnath, and Badrinath, collectively known as Char Dham. These pilgrimage centres draw large number of pilgrims each year, thus becoming the most important hubs of religious travel in the whole of Northern India. \n" +
-                        "\n" +
-                        "Badrinath is located at an elevation of around 3,100 m. Located in the Garhwal Himalayas, on the banks of the Alaknanda river, this sacred town lies between Nar and Narayana mountain ranges. The temple is believed to have been established by sage Adi Shankaracharya in the 8th century. With Lord Vishnu as its presiding deity, the temple remains open for six months in a year. In winter it becomes inaccessible due to heavy snowfall.\n" +
-                        "\n" +
-                        "BEST TIME TO VISIT\n" +
-                        "\n" +
-                        "The ideal time to visit Badrinath Temple is from May to October. The temple is closed from October/ November, and is reopened around April.\n" +
-                        "\n" +
-                        "Where To Stay\n" +
-                        "\n" +
-                        "Badrinath (Yatri Niwas)"));
+                startActivity(new Intent(getApplicationContext(), Details.class)
+                        .putExtra("NAME", "Badrinath")
+                        .putExtra("CONTENT", "The Badrinath Temple also known as the Badrinarayan Temple, located in Uttarakhand's Badrinath town, is one of the four Char Dhams (four important pilgrimages) in the state. There are four pilgrim-destinations namely Yamunotri, Gangotri, Kedarnath, and Badrinath, collectively known as Char Dham. These pilgrimage centres draw large number of pilgrims each year, thus becoming the most important hubs of religious travel in the whole of Northern India. \n" +
+                                "\n" +
+                                "Badrinath is located at an elevation of around 3,100 m. Located in the Garhwal Himalayas, on the banks of the Alaknanda river, this sacred town lies between Nar and Narayana mountain ranges. The temple is believed to have been established by sage Adi Shankaracharya in the 8th century. With Lord Vishnu as its presiding deity, the temple remains open for six months in a year. In winter it becomes inaccessible due to heavy snowfall.\n" +
+                                "\n" +
+                                "BEST TIME TO VISIT\n" +
+                                "\n" +
+                                "The ideal time to visit Badrinath Temple is from May to October. The temple is closed from October/ November, and is reopened around April.\n" +
+                                "\n" +
+                                "Where To Stay\n" +
+                                "\n" +
+                                "Badrinath (Yatri Niwas)"));
             }
         });
 
     }
+
+    private void getCurrentLoca() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        lat = location.getLatitude();
+                        log = location.getLongitude();
+                    }
+                }
+            });
+        }
+    }
+
 }
