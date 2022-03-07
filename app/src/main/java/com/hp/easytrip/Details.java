7f0a2df56d07b5784b7d974402eb45268b33469b
Aswin Hariram <Aswin_pc_hp@outlook.com>
2022-03-07 18:26:29 +0530

    aswin commit

---
+++ app/src/main/java/com/hp/easytrip/Details.java
@@ -0,0 +1,35 @@
+package com.hp.easytrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hp.easytrip.databinding.ActivityDetailsBinding;
+
+public class Details extends AppCompatActivity {
+
+    ActivityDetailsBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String name = getIntent().getStringExtra("NAME");
+        String content = getIntent().getStringExtra("CONTENT");
+
+        binding.collaping.setTitle(name);
+        binding.content.setText(content);
+        switch (name){
+            case "Badrinath":
+                binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.badrniath));
+                break; case "Gangotri":
+                binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.gangotri));
+                break; case "Yamunotri":
+                binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.yamunotrie));
+                break; case "Kedarnath":
+                binding.backImg.setImageDrawable(getResources().getDrawable(R.drawable.khendra));
+                break;
+        }
+    }
+}
--- app/src/main/java/com/hp/easytrip/police_nearby.java
+++ app/src/main/java/com/hp/easytrip/police_nearby.java
@@ -1,22 +1,17 @@
 package com.hp.easytrip;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,11 +23,23 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.hp.easytrip.databinding.ActivityPoliceNearbyBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class police_nearby extends FragmentActivity implements OnMapReadyCallback {
 
+    FusedLocationProviderClient client;
+    double lat, log;
     private GoogleMap mMap;
     private ActivityPoliceNearbyBinding binding;
-    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +49,11 @@ public class police_nearby extends FragmentActivity implements OnMapReadyCallbac
         setContentView(binding.getRoot());
 
 
-        client =    LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
-         getCurrentLoca();
-        }
-        else {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},101);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLoca();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
 
         }
 
@@ -59,7 +65,7 @@ public class police_nearby extends FragmentActivity implements OnMapReadyCallbac
     }
 
     private void getCurrentLoca() {
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
 
             Task<Location> task = client.getLastLocation();
@@ -67,6 +73,8 @@ public class police_nearby extends FragmentActivity implements OnMapReadyCallbac
                 @Override
                 public void onSuccess(Location location) {
                     if (location != null) {
+                        lat = location.getLatitude();
+                        log = location.getLongitude();
                         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                         MarkerOptions options = new MarkerOptions().position(latLng).title("Current Postion");
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
@@ -84,8 +92,23 @@ public class police_nearby extends FragmentActivity implements OnMapReadyCallbac
 
         // Add a marker in Sydney and move the camera
 
-       getCurrentLoca();
+        getCurrentLoca();
+
+        StringBuilder builder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        builder.append("location="+lat+","+log);
+        builder.append("&radius=1000");
+        builder.append("&keyword="+"atm");
+        builder.append("&sensor=true");
+        builder.append("&key="+getResources().getString(R.string.KEY));
+
+        String url = builder.toString();
+        Object data[] = new Object[2];
+        data[0]=mMap;
+        data[1]=url;
+        FetchData fetchData =new FetchData();
+        fetchData.execute(data);
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -93,6 +116,79 @@ public class police_nearby extends FragmentActivity implements OnMapReadyCallbac
         if (requestCode == 101) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getCurrentLoca();
+            }
+        }
+    }
+
+    class DownloadURL {
+        public String retriveurl(String url) throws IOException {
+            String urlData = "";
+            HttpURLConnection httpURLConnection = null;
+            InputStream inputStream = null;
+            try {
+                URL getUrl = new URL(url);
+                httpURLConnection = (HttpURLConnection) getUrl.openConnection();
+                httpURLConnection.connect();
+                inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer sb = new StringBuffer();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+                urlData = sb.toString();
+                bufferedReader.close();
+            } catch (Exception e) {
+                Log.e("TAG", "retriveurl: " + e);
+            } finally {
+                inputStream.close();
+                httpURLConnection.disconnect();
+            }
+            return urlData;
+        }
+    }
+
+    class FetchData extends AsyncTask<Object, String, String> {
+        String googleNearbyData;
+        String url;
+        GoogleMap map;
+
+        @Override
+        protected String doInBackground(Object... objects) {
+            try {
+                map = (GoogleMap) objects[0];
+                url = (String) objects[1];
+                DownloadURL downloadURL = new DownloadURL();
+                googleNearbyData = downloadURL.retriveurl(url);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return googleNearbyData;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject object = new JSONObject(s);
+                JSONArray jsonArray = object.getJSONArray("results");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    JSONObject getLOC = jsonObject.getJSONObject("geometry")
+                            .getJSONObject("location");
+                    String lat = getLOC.getString("lat");
+
+                    String lang = getLOC.getString("lng");
+
+                    JSONObject getname =jsonArray.getJSONObject(i);
+                    String name = getname.getString("name");
+
+                    LatLng latLng = new LatLng(Double.parseDouble(lat),Double.parseDouble(lang));
+                    MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(name);
+                    map.addMarker(markerOptions);
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
     }
