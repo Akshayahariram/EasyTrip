fba080c0ac1c5c94cc03ce79262da3616f00350d
Aswin Hariram <Aswin_pc_hp@outlook.com>
2022-03-07 14:51:03 +0530

    aswin commit

--- app/src/main/java/com/hp/easytrip/MainActivity.java
+++ app/src/main/java/com/hp/easytrip/MainActivity.java
@@ -2,13 +2,27 @@ package com.hp.easytrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.hp.easytrip.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.policeNearby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),police_nearby.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/hp/easytrip/hotels_nearby.java
@@ -0,0 +1,14 @@
+package com.hp.easytrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hotels_nearby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotels_nearby);
+    }
+}
